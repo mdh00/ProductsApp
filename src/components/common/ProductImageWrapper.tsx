81bfb3b9db2fbd72264ea608ba2cb2a08b3e09b0
Madhini Maheshika <127973368+mdh00@users.noreply.github.com>
2024-07-23 21:55:32 +0530

    delete unwanted

--- src/components/common/ProductImageWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import NextImage, { type ImageProps } from "next/image";
-
-export const ProductImageWrapper = (props: ImageProps) => {
-	return (
-		<div>
-			<NextImage {...props} className="h-full w-full object-contain object-center mb-2" />
-		</div>
-	);
-};
