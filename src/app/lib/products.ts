14525b2c01db1f047b94b53151193dbcbab420de
Madhini Maheshika <127973368+mdh00@users.noreply.github.com>
2024-07-24 22:29:35 +0530

    bug/api

--- src/app/lib/products.ts
+++ src/app/lib/products.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchProducts = async () => {
-  const response = await axios.get('/lib/products');
+  const response = await axios.get('/api/productsApi');
   return response.data;
 };
