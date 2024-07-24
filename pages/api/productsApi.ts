c9d458c24c929fcc6dbfc1ff95c5b51c2bfcfd68
Madhini Maheshika <127973368+mdh00@users.noreply.github.com>
2024-07-24 23:21:26 +0530

    fixed/mogodb

--- pages/api/productsApi.ts
+++ pages/api/productsApi.ts
@@ -5,9 +5,9 @@ import clientPromise from '@/app/lib/mongodb';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const client = await clientPromise;
-    const db = client.db();
+    const db = client.db('ProductDB');
 
-    const products = await db.collection('products').find({}).toArray();
+    const products = await db.collection('product').find({}).toArray();
 
     res.status(200).json(products);
   } catch (error) {
